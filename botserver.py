99a25b28752f0b01283862685c6e305e8bf93034
Newbie1379 <comza962@gmail.com>
2024-10-03 18:44:54 +0700

    Add files via upload

--- botserver.py
+++ botserver.py
@@ -1,15 +1,15 @@
-# from flask import Flask
-# from threading import Thread
+from flask import Flask
+from threading import Thread
 
-# app = Flask('')
+app = Flask('')
 
-# @app.route('/')
-# def home():
-#     return "Server is running!"
+@app.route('/')
+def home():
+    return "Server is running!"
 
-# def run():
-#   app.run(host='0.0.0.0',port=8080)
+def run():
+   app.run(host='0.0.0.0',port=8080)
 
-# def server_on():
-#     t = Thread(target=run)
-#     t.start()
+def server_on():
+     t = Thread(target=run)
+#    t.start()
